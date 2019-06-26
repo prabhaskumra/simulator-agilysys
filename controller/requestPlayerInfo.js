bb2d0fc4db537bcc74ab917ffb5d17d70b536eed
daee kang <44410043+daeeKang@users.noreply.github.com>
2019-06-26 16:08:41 -0700

    take in json entry and query search with that

--- controller/requestPlayerInfo.js
+++ controller/requestPlayerInfo.js
@@ -2,9 +2,32 @@
 function submitSearch(){
     let searchValue = document.getElementById('player-request-text-box').value
     let account = searchPlayers(searchValue)
-    console.log(players)
-    console.log(account)
+
 }
+
+function submitJSON(){
+    let dialog = remote.dialog
+    dialog.showOpenDialog({
+        title: 'Open JSON file',
+        filters: [
+            {name: 'text', extensions: ['txt']}
+        ]
+    }, (fileName) => {
+        if(fileName === undefined){
+            return
+        }
+        fs.readFile(fileName[0], 'utf8', (err, data) => {
+            if(err){
+                console.log(err)
+            } else {
+                let inRequest = JSON.parse(data)
+                console.log(inRequest["acct"])
+                console.log(searchPlayers(inRequest["acct"]))
+            }
+        })
+    })
+}
+
 
 function searchPlayers(id){
     for (var i=0; i < players.length; i++) {
