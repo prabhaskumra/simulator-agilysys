b86ca6988e81df32bb3d1a6290aad7e7efe793a1
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-06-26 14:07:53 -0700

    updated the parse fuction
    1. Renamed the objects array to players
    2. moved all the csv delaration inside the parsing function

--- js/dataread.js
+++ js/dataread.js
@@ -1,12 +1,11 @@
 const csv = require('csv-parser')
 const { remote } = require('electron')
 const fs = require('fs')
-var results = [];
+var players = [];
 
-csv(['firstName', 'lastName', 'accountNumber', 'tierLevel', 'pointBalance']);
-csv({ separator: ',' });
 
 function openFile(){
+
     let dialog = remote.dialog
     dialog.showOpenDialog({
         title: 'Open Mock Data',
@@ -15,20 +14,20 @@ function openFile(){
         ]
     }, (fileName) => {
         if(fileName === undefined) return; //file not picked
-
         var file = fileName[0]
-        parseCSVData(fileName[0])
+        parseCSVData(file)
     })
 }
 
 function parseCSVData(file) {
+    csv(['firstName', 'lastName', 'accountNumber', 'tierLevel', 'pointBalance']);
     csv({ separator: ',' });
 
     fs.createReadStream(file)
         .pipe(csv())
-        .on('data', (data) => results.push(data))
+        .on('data', (data) => players.push(data))
         .on('end' , () => {
-            //console.log(results);
+            //console.log(players);
         });
 
 }
