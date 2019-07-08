7c1e31481d90e3469eb92a4b5e68e14b6275edd7
daee kang <44410043+daeeKang@users.noreply.github.com>
2019-07-08 16:03:34 -0700

    clean up server.js

--- server.js
+++ server.js
@@ -1,127 +1,127 @@
-   
-    //other shit
-    const express = require('express')
-    const app = express()
-    const port = 1234
-    const path = require('path')
-    const bodyParser = require('body-parser'); 
-    const fs = require('fs')
-    const { ipcMain } = require('electron')
+//other shit
+const express = require("express");
+const app = express();
+const port = 1234;
+const path = require("path");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const { ipcMain } = require("electron");
 
+//-------------------------------------model require-------------------------------------//
+const getPlayerInfo = require("./model/GetPlayerInfo").getPlayerInfo;
+const GetOffers = require("./model/GetOffers").GetOffers;
+const RedeemComp = require("./model/RedeemComp").RedeemComp;
+const RedeemPoints = require("./model/RedeemPoints").RedeemPoints;
+//---------------------------------------------------------------------------------------//
 
-    //-------------------------------------model require-------------------------------------//
-    const getPlayerInfo = require('./model/GetPlayerInfo').getPlayerInfo
-    const GetOffers = require('./model/GetOffers').GetOffers
-    const RedeemComp = require('./model/RedeemComp').RedeemComp
-    const RedeemPoints = require('./model/RedeemPoints').RedeemPoints
-    //---------------------------------------------------------------------------------------//
+//------------------------------------express setup--------------------------------------//
+app.use(express.static(__dirname));
 
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
+//---------------------------------------------------------------------------------------//
 
-    //------------------------------------express setup--------------------------------------//
-    app.use(express.static(__dirname));
+//this below code recieves the appReady state as in, is data loaded and ready?
+//api calls will return errors if data is not loaded. dab
+var appReady = false;
+ipcMain.on("isAppReady", (event, isAppReady) => {
+  appReady = isAppReady;
+});
 
-    app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
-    app.use(bodyParser.json()); // support json encoded bodies
-    //---------------------------------------------------------------------------------------//
+//default load page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname + "/views/index.html"))
+);
 
-    //this below code recieves the appReady state as in, is data loaded and ready? 
-    //api calls will return errors if data is not loaded. dab
-    var appReady = false
-    ipcMain.on("isAppReady", (event, isAppReady) => {
-        appReady = isAppReady
-    })
+//validate user api Post
+//IG will send post request and get account back - also will display account information
+app.post("/GetPlayerInfo", (req, res) => {
+  //get account number and search by acct number
+  if (!appReady) {
+    res.send({ error: "data not loaded" });
+    return;
+  }
+  let account = getPlayerInfo(req.body.acct);
+  //send back account info
+  res.send(account ? account : { "error:": "no results" });
+  //write found account to json file and electron window will load it
+  writeTransaction(
+    {
+      model: GetOffers
+    },
+    account
+  );
+});
 
+//returns all offers from offers.json that match the account number
+app.post("/GetOffers", (req, res) => {
+  if (!appReady) {
+    res.send({ error: "data not loaded" });
+    return;
+  }
+  let offers = GetOffers(req.body.AccountNumber);
+  let account = getPlayerInfo(req.body.AccountNumber);
+  res.send(offers);
+  writeTransaction(
+    {
+      model: GetOffers
+    },
+    account
+  ); //should display player info anyways
+});
 
-    //default load page
-    app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/views/index.html')))
+//redeem each comp in list, i am assuming that there could be more than one
+app.post("/RedeemComp", (req, res) => {
+  if (!appReady) {
+    res.send({ error: "data not loaded" });
+    return;
+  }
 
-    //validate user api Post
-    //IG will send post request and get account back - also will display account information
-    app.post('/GetPlayerInfo', (req, res) => {
-        //get account number and search by acct number
-        if(!appReady){
-            res.send({error: "data not loaded"})
-            return
-        }
+  let compList = req.body.RedeemCompList;
+  let redeemValues = RedeemComp(req.body.AccountNumber, compList);
+  res.send(redeemValues.out);
+  let newCompValue = redeemValues.out.CompBalance;
+  writeTransaction({
+    model: "RedeemComp",
+    compBalance: newCompValue,
+    accountNumber: req.body.AccountNumber,
+    redeemedAmount: redeemValues.redeemedTotal
+  });
+});
 
+app.post("/RedeemPoints", (req, res) => {
+  if (!appReady) {
+    res.send({ error: "data not loaded" });
+    return;
+  }
 
-        accountNumber = req.body.acct
-        let account = getPlayerInfo(accountNumber)
-        //write found account to json file and electron window will load it
-        writeTransaction({
-            "model": GetOffers
-        }, account)
-        //send back account info
-        res.send(account ? account : {"error:": "no results"})
-    })
+  let redeemPointsList = req.body.RedeemPointsList;
 
-    //returns all offers from offers.json that match the account number
-    app.post('/GetOffers', (req, res) => {
-        if(!appReady){
-            res.send({error: "data not loaded"})
-            return
-        }
-        let accountNumber = req.body.AccountNumber
-        let offers = GetOffers(accountNumber)
-        let account = getPlayerInfo(accountNumber)
-        writeTransaction({
-            "model": GetOffers
-        }, account)//should display player info anyways
+  let redeemValues = RedeemPoints(req.body.AccountNumber, redeemPointsList);
+  res.send(redeemValues.out);
 
-        res.send(offers)
-    })
+  writeTransaction({
+    model: "RedeemPoints",
+    pointBalance: redeemValues.out.PointsBalance,
+    accountNumber: req.body.AccountNumber,
+    redeemedAmount: redeemValues.redeemedTotal
+  });
+});
 
-    //redeem each comp in list, i am assuming that there could be more than one
-    app.post('/RedeemComp', (req, res) => {
-        if(!appReady){
-            res.send({error: "data not loaded"})
-            return
-        }
+app.listen(port, () => console.log(`server running on port ${port}`));
 
-        let accountNumber = req.body.AccountNumber
-        let compList = req.body.RedeemCompList
-        let redeemValues = RedeemComp(accountNumber, compList)
-        res.send(redeemValues.out)
-        let newCompValue = redeemValues.out.CompBalance
-        writeTransaction({
-            "model": "RedeemComp",
-            "compBalance" : newCompValue,
-            "accountNumber" : accountNumber,
-            "redeemedAmount": redeemValues.redeemedTotal
-        })
-    })
-
-    app.post('/RedeemPoints', (req, res) => {
-        if(!appReady){
-            res.send({error: "data not loaded"})
-            return
-        }
-
-        let accountNumber = req.body.AccountNumber
-        let redeemPointsList = req.body.RedeemPointsList
-
-        let redeemValues = RedeemPoints(accountNumber, redeemPointsList)
-        res.send(redeemValues.out)
-
-        writeTransaction({
-            "model": "RedeemPoints",
-            "pointBalance" : redeemValues.out.PointsBalance,
-            "accountNumber" : accountNumber,
-            "redeemedAmount": redeemValues.redeemedTotal
-        })
-    })
-
-
-    app.listen(port, () => console.log(`server running on port ${port}`))
-
-    function writeTransaction(transaction, account){
-        let transactionData = {
-            "transaction": transaction,
-            "account": account
-        }
-        fs.writeFile('transaction.json', JSON.stringify(transactionData), 'utf8', (err) => {
-            if(err) throw err
-        })
+//writes transaction to file so that the main process (electron) can pick up what was done
+function writeTransaction(transaction, account) {
+  let transactionData = {
+    transaction: transaction,
+    account: account
+  };
+  fs.writeFile(
+    "transaction.json",
+    JSON.stringify(transactionData),
+    "utf8",
+    err => {
+      if (err) throw err;
     }
-
-
+  );
+}
