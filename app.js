ffb2f49228bdaf8a87986ec15a8fe7b4c3cc796b
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-10 16:22:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const server = require('./server')
 const fs = require('fs')
 const path = require('path')
@@ -33,4 +33,20 @@ app.on('ready', () => {
       console.log("deleted transaction.json")
     })
   })
+})
+
+ipcMain.on("editUser", (event) => {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    //frame: false,
+    minWidth: 800,
+    minHeight:600,
+    icon: './image_assets/spades.png',
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+  // and load the index.html of the app.
+    win.loadURL('http://localhost:1234/editUser')
 })
