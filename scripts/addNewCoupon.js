a374888dee0f3e9482f7cb05e9ee41fb735f5ea1
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-29 15:03:22 -0700

    Add new coupon (#59)
    * open a new window
    
    can open a new window to add new coupon
    
    * adding value to the form
    
    * now admin can add new coupon data
    
    * final touch

---
+++ scripts/addNewCoupon.js
@@ -0,0 +1,69 @@
+var couponData = db.get('coupons').value()
+var newCoupon = {}
+var couponFlags = {}
+
+function addNewCoupon(){
+
+    newCoupon = {
+            "CouponNumber": "",
+            "Balance": ""
+    }
+
+    couponFlags = {
+        "CouponNumber": false,
+        "Balance": false,
+      }
+
+}
+
+function saveCouponForm(){
+
+    couponData.push(newCoupon)
+    db.set('players', playerData).write()
+    updateCouponTable()
+    addNewCoupon()
+    checkCouponFlag()
+
+    document.getElementById('save-coupon').textContent = "New Coupon Added Successfully!";
+
+    setTimeout(function(){
+        document.getElementById('save-coupon').textContent = "";
+    },3000);
+
+    document.getElementById("coupon-form").reset();
+}
+
+
+function enterCouponInfo(inputElement){
+    
+    if(inputElement.name === 'couponNumber'){
+        newCoupon.CouponNumber = inputElement.value
+        couponFlags.CouponNumber = true
+        checkCouponFlag()
+    }
+    else if(inputElement.name === 'balance'){
+        newCoupon.Balance = inputElement.value
+        couponFlags.Balance = true
+        checkCouponFlag() 
+    }
+}
+
+function checkCouponFlag(){
+    
+    if(couponFlags.CouponNumber && couponFlags.Balance)
+        document.getElementById("save-coupon-button").disabled = false
+    else
+        document.getElementById("save-coupon-button").disabled = true
+}
+
+function newCouponWindow(){
+    document.getElementById('add-coupon').style.display = 'block'
+    document.getElementById('edit-coupons').style.display = 'none'
+    addNewCoupon()
+    document.getElementById("save-coupon-button").disabled = true
+}
+
+function loadCouponTable(){
+    document.getElementById('add-coupon').style.display = 'none'
+    document.getElementById('edit-coupons').style.display = 'block'
+}
